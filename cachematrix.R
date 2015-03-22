b43067e54676fb1670115914cc1af0d99ff6ce1d
samward81 <samward81@yahoo.com>
2015-03-21 19:20:40 -0600

    Created functions
    removed template information and added working functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Functions to store the inverse of a matrix in the cache 
+# this function creates a matrix that is stored in the cache  
+makeCacheMatrix <- function(x = matrix()){
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) inv <<- solve
+    getinverse <- function() inv
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+    
 }
 
-
-## Write a short comment describing this function
-
+# this function takes finds the inverse of the matrix and caches the value
+# if the inverse is already in the cache it will pull the cached value 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    
 }
